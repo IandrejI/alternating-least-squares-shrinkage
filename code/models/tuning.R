d4d9d93eea73bea13496a0c2f430f2610fbe4b13
|andrej| <andrej.muschke@stud.ku.de>
2022-07-05 17:35:59 +0200

    update grid search

--- code/models/tuning.R
+++ code/models/tuning.R
@@ -4,11 +4,14 @@ source("code/models/ALS.R")
 
 # 10-CV -------------------------------------------------------------------
 tuning.grid <- expand.grid(impRate = seq(0.0001,0.001,0.0001), alpha = seq(0,1,0.05),
-                          lambda = seq(0,10,0.25), dim = seq(10,50,5), RMSE.Valid = NA)
+                          lambda = seq(0,5,0.1), dim = seq(10,50,5), RMSE.Valid = NA)
 
 set.seed(385)
-sample.grid <- tuning.grid[sample(1:nrow(tuning.grid), 3),]
+sample.grid <- tuning.grid[sample(1:nrow(tuning.grid), 1000),]
 
+
+
+start <- Sys.time()
 for(i in 2:nrow(sample.grid)){
     impRate <- sample.grid$impRate[i]
     alpha <- sample.grid$alpha[i]
@@ -24,8 +27,8 @@ for(i in 2:nrow(sample.grid)){
 
     print(paste0("grid-progress: ",i/nrow(sample.grid)*100, "%"))
 }
+end <- Sys.time()
 
-
-
+duration <- end-start
 
 
